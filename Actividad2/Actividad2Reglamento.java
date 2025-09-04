0db48be1f10890bb3e8fe77746f4cd331f0a04dc
124049707-dot <124049707@upq.edu.mx>
2025-09-04 15:05:37 -0600

    Se termino la primera parte solicitada, el reglamento

---
+++ Actividad2/Actividad2Reglamento.java
@@ -0,0 +1,47 @@
+import javax.swing.JFrame;
+public class PAM {
+
+public void ReglamentoPOO(){
+JOptionPane.showMessageDialog("Este es el reglamento para la clase de POO");
+JoptionPane.showMessageDialog("\n 1.-Se requieren 80% de asistencia para tener derecho a evaluacion de trabajos en clase.
+\n2.-Se permiten 10 minutos de tolerancia y si el alumno llega despues de este tiempo puede permaneceren la clase, pero no se tomara la asistencia en clase (Solamente en los
+horarios de inicio 7:00 am y 14:00 pm)
+\n3. Las faltas deberan estar justificadas mediante el correo institucional con un plazo maximo de 24 horas posteriores a la hora de falta en clase mediante correo del tutor (a),
+justificantes entregados fuera de la fecha limite permitido no se aceptan, unicamente se aceptaran recetas medicas y citatorios juridicos.(De forma fisica deben ser presentados al
+tutor para ser validados y de forma posterior emitidos).
+\n4. Las tareas y trabajos deberan subirlas al Classroom de forma individual y no se recibiran de manera extemporanea.(Salvo previo justificante validado por el tutor)
+\n5. Las tareas y trabajos presentarlos en tiempo y forma. La demora de un trabajo o tarea sin justificante y/o con justificante fuera del limite no se aceptan.
+\n6. Utilizar el correo institucional para trabajar bajo la plataforma Google Classroom.
+\n7. El plagio o copia de trabajos y/o examenes, sera condicionado a reprobar a la asignatura y se reporta al area correspondiente.
+\n8. Cualquier deshonestidad academica sera sancionada reprobando el parcial sin derecho a examen final
+\n9. En circunstancia de indisciplina o falta de respeto por parte del alumno hacia docentes,administrativos, companeros o cualquier persona perteneciente a la universidad, se realizara
+una primera llama de atencion, si el alumno hace caso omiso tendra que abandonar el aula, tres incidencias de este tipo el alumno no tendra derecho a examen final o parcial.
+\n10. Uso de laptops y/o dispositivos moviles quedara limitados a uso exclusivo de las actividades que asi lo requieran.
+\n11. Prohibido el uso de audifonos durante la clase.
+\n12. Prohibido comer y/o tomar liquidos en el salon.
+\n13. Prohibido sentarse encima de las mesas , asi como columpiarse en las sillas.
+\n14.Todo tema academido debe ser revisado primeramente por parte del alumno con el docente, de no resolverse, pasar con su respectivo tutor, y de ser necesario con la coordinacion
+de tutores. En caso de no solucionarse pasar a la direccion del programa educativo (debe mantenerse este seguimiento de forma obligatoria)
+\n15.Cualquier situacion no prevista en el presente reglamento pasar directamente con la direccion del programa educativo.
+\n16. El dia destinado a entrega de calificaciones todos los estudiantes deben estar presentes, ese dia se entregaran examenes y se brindara retroalimentacion
+\n17.Este reglamento entra en vigor despues de que se firme o se acepte por la mayoria de los estudiantes asistentes a la primera sesion de la materia, una vez firmado o aceptado por el
+50% mas el jefe de grupo, es vigente para todo alumno inscrito en el curso aunque no este presente en la primera sesion.
+
+");
+}
+public void LineamientosClassroom(){
+
+}
+public void FechasDeParciales(){
+
+}
+public void PorcentajesPorParcial(){
+
+}
+
+}
+
+public static void main(String[] args){
+    PAM reglas = new reglas();
+
+}
