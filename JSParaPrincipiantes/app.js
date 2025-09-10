c4b066efae64b1cdb5c1fa9dba9ef5a135ee7e86
124049707-dot <124049707@upq.edu.mx>
2025-09-10 00:33:00 -0600

    uso del condicional switch

--- JSParaPrincipiantes/app.js
+++ JSParaPrincipiantes/app.js
@@ -1,10 +1,11 @@
-let score = 70;
-if (score > 30){
-    console.log('You need to practice more');
-
-}else if(score > 15){
-    console.log('Estas mejorando');
-}
-else{
-    console.log('You need to follow this tutorial');
+let typecard = 'Debid card';
+switch(typecard){
+    case'Debid card':
+    console.log('This is a debid card');
+    break;
+     case'Credit card':
+    console.log('This is a credit card');
+    break;
+    default:
+        console.log('No card');
 }
