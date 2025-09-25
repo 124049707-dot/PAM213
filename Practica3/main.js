025dd5dbf7cb32426c4215425db803f6e37cb76c
124049707-dot <124049707@upq.edu.mx>
2025-09-25 14:49:34 -0600

    se realizo el codigo para el segundo punto del inciso a

---
+++ Practica3/main.js
@@ -0,0 +1,9 @@
+import {restar} from './utils.js';
+console.log("Prueba 1");
+console.log(restar(10,5));
+console.log("Prueba 2");
+console.log(restar(100,15));
+console.log("Prueba 3");
+console.log(restar(3,2));
+
+
--- Practica3/utils.js
+++ Practica3/utils.js
@@ -1,3 +1,3 @@
-export function resta (a,b){
-    return a-b;
+export function restar (a, b) {
+    return a - b;
  }
