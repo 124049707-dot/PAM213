43926995ecffd2b4b3fc9a6206dc2803e4d3dd00
124049707-dot <124049707@upq.edu.mx>
2025-10-08 20:16:13 -0600

    se finalizo la practica 5 en la que se vieron los temas de clase

--- intro/App.js
+++ intro/App.js
@@ -1,15 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
+//1.- Imports: Zona de importaciones
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import Reactimport, { useState } from 'react';
+
+
+//2.-Main: Zona de componentes
 export default function App() {
+  const [contador,setContador]=useState(0); //Desestructuración, aqui se inicia la variable
   return (
+    // debe estar dentro de una etiqueta view para ser visible en mi screen
     <View style={styles.container}>
-      <Text>Hola Mundo React Native!!</Text>
-      <StatusBar style="auto" />
-    </View>
+      
+      <Text>Contador:{contador} </Text> {/* comentarios con shit+alt+a */}
+        {/* //<Button></Button> */}
+        <Button title="Agregar" onPress={()=>setContador(contador+1)}/> {/* funcion flecha, apertura de autocierre, cosas mas simples */}
+        <Button title="Quitar" onPress={()=>setContador(contador-1)}/>
+        <Button title="Reiniciar" onPress={()=>setContador(0)}/>
+        <StatusBar style="auto" /> 
+        
+      </View>
+    
   );
 }
 
+//3.- Estilos: Zona de estetica y posicionamiento 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
